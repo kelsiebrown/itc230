4b080c38664313b21d45bae26b1cbd5bf1700f94
kelsiebrown <kelsie.brown@seattlecentral.edu>
2018-06-28 17:33:37 -0700

    Add assignment 1 files

---
+++ index.js
@@ -0,0 +1,20 @@
+//index.js for week 1 assignment
+
+var http = require('http');
+http.createServer(function(req,res){
+    var path = req.url.toLowerCase();
+    switch(path){
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Welcome to the home page!');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('This is the about page. Some content about my app will go here later.');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('This file does not exist!');
+            break;
+    }
+}).listen(process.env.PORT || 3000);

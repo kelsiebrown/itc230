018d8ebfe2b87d684c9aff4bdac8aeae6359454f
Kelsie Brown <38116231+kelsiebrown@users.noreply.github.com>
2018-07-17 11:10:41 -0700

    Add assignment 3 files

--- index.js
+++ index.js
@@ -1,61 +1,56 @@
-//index.js for ITC230
+// index.js for ITC230
 
-var http = require("http"), fs = require('fs'), qs = require('querystring');
+'use strict'
+
 let albums = require("./lib/albums.js");
 
-function serveStatic(res, path, contentType, responseCode){
-  if(!responseCode) responseCode = 200;
-  console.log(__dirname + path)
-  fs.readFile(__dirname + path, function(err, data){
-      if(err){
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-      }
-      else{
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-      }
-  });
-}
+// set up express
+const express = require("express");
+const app = express();
 
-http.createServer(function(req,res){
-  let url = req.url.split("?");
-  let query = qs.parse(url[1]);
-  let path = url[0].toLowerCase();
-    
-  switch(path) {
-    case '/': 
-      serveStatic(res, '/public/home.html', 'text/html');
-      break;
-          
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('This is the about page');
-      break;
-          
-    case '/get':
-      let found = albums.get(query.title);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results = (found) ? JSON.stringify(found) : "Not found";
-      res.end('Results for ' + query.title + "\n" + results);
-      break;
-          
-    case '/delete':
-      let deleted = albums.delete(query.title);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results_deleted = (deleted.deleted) ? ' was deleted.' : ' was not deleted.';
-      res.end(query.title + results_deleted);
-      break;
-          
-    case '/add':
-      let added = albums.add(query.title);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results_added = (added.added) ? ' was added.' : ' was not added.';
-      res.end(query.title + results_added);
-      break;
-          
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404: Page not found.');
-  }
-}).listen(process.env.PORT || 3000);
+// set port to 3000
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.use(require("body-parser").urlencoded({extended: true}));
+
+// set up handlebars
+let handlebars = require("express-handlebars");
+app.engine(".html", handlebars({extname: ".html"}));
+app.set("view engine", ".html");
+
+// static file for home.html
+app.get('/', (req, res) => {
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html');
+});
+
+// plain text response for about page
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('This is the about page');
+});
+
+// GET response for delete path
+app.get('/delete', (req, res) => {
+    let deleted = albums.delete(req.query.title);
+    res.render("delete", {title: req.query.title, deleted: deleted});
+});
+
+// POST response for get path
+app.post('/get', (req, res) => {
+    console.log(req.body);
+    let found = albums.get(req.body.title);
+    res.render("details", {title: req.body.title, found: found});
+});
+
+// USE response for 404 handler
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Page Not Found');
+});
+
+// start server
+app.listen(app.get('port'), () => {
+    console.log('Express started');
+});
